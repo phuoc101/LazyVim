eb51282469da19fa199f57a4c8d9e5a1f2001b48
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2023-04-11 23:21:14 +0300

    update path

--- lua/lazyvim/plugins/core.lua
+++ lua/lazyvim/plugins/core.lua
@@ -2,5 +2,5 @@ require("lazyvim.config").init()
 
 return {
   { "folke/lazy.nvim", version = "*" },
-  { "phuoc101/LazyVim", priority = 10000, lazy = false, config = true, cond = true, version = "*" },
+  { "git@github.com:phuoc101/LazyVim.git", priority = 10000, lazy = false, config = true, cond = true, version = "*" },
 }
