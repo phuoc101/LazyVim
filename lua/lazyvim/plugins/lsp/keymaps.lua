efc578311905e9b00754dcb63750e8af3e8e3c09
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2023-04-18 14:37:59 +0300

    removed default keymaps

--- lua/lazyvim/plugins/lsp/keymaps.lua
+++ lua/lazyvim/plugins/lsp/keymaps.lua
@@ -10,40 +10,40 @@ function M.get()
   ---@class PluginLspKeys
     -- stylua: ignore
     M._keys =  {
-      { "<leader>cd", vim.diagnostic.open_float, desc = "Line Diagnostics" },
-      { "<leader>cl", "<cmd>LspInfo<cr>", desc = "Lsp Info" },
-      { "gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Goto Definition", has = "definition" },
-      { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References" },
-      { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration" },
-      { "gI", "<cmd>Telescope lsp_implementations<cr>", desc = "Goto Implementation" },
-      { "gt", "<cmd>Telescope lsp_type_definitions<cr>", desc = "Goto Type Definition" },
-      { "K", vim.lsp.buf.hover, desc = "Hover" },
-      { "gK", vim.lsp.buf.signature_help, desc = "Signature Help", has = "signatureHelp" },
-      { "<c-k>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "signatureHelp" },
-      { "]d", M.diagnostic_goto(true), desc = "Next Diagnostic" },
-      { "[d", M.diagnostic_goto(false), desc = "Prev Diagnostic" },
-      { "]e", M.diagnostic_goto(true, "ERROR"), desc = "Next Error" },
-      { "[e", M.diagnostic_goto(false, "ERROR"), desc = "Prev Error" },
-      { "]w", M.diagnostic_goto(true, "WARN"), desc = "Next Warning" },
-      { "[w", M.diagnostic_goto(false, "WARN"), desc = "Prev Warning" },
-      { "<leader>cf", format, desc = "Format Document", has = "documentFormatting" },
-      { "<leader>cf", format, desc = "Format Range", mode = "v", has = "documentRangeFormatting" },
-      { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v" }, has = "codeAction" },
-      {
-        "<leader>cA",
-        function()
-          vim.lsp.buf.code_action({
-            context = {
-              only = {
-                "source",
-              },
-              diagnostics = {},
-            },
-          })
-        end,
-        desc = "Source Action",
-        has = "codeAction",
-      }
+      -- { "<leader>cd", vim.diagnostic.open_float, desc = "Line Diagnostics" },
+      -- { "<leader>cl", "<cmd>LspInfo<cr>", desc = "Lsp Info" },
+      -- { "gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Goto Definition", has = "definition" },
+      -- { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References" },
+      -- { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration" },
+      -- { "gI", "<cmd>Telescope lsp_implementations<cr>", desc = "Goto Implementation" },
+      -- { "gt", "<cmd>Telescope lsp_type_definitions<cr>", desc = "Goto Type Definition" },
+      -- { "K", vim.lsp.buf.hover, desc = "Hover" },
+      -- { "gK", vim.lsp.buf.signature_help, desc = "Signature Help", has = "signatureHelp" },
+      -- { "<c-k>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "signatureHelp" },
+      -- { "]d", M.diagnostic_goto(true), desc = "Next Diagnostic" },
+      -- { "[d", M.diagnostic_goto(false), desc = "Prev Diagnostic" },
+      -- { "]e", M.diagnostic_goto(true, "ERROR"), desc = "Next Error" },
+      -- { "[e", M.diagnostic_goto(false, "ERROR"), desc = "Prev Error" },
+      -- { "]w", M.diagnostic_goto(true, "WARN"), desc = "Next Warning" },
+      -- { "[w", M.diagnostic_goto(false, "WARN"), desc = "Prev Warning" },
+      -- { "<leader>cf", format, desc = "Format Document", has = "documentFormatting" },
+      -- { "<leader>cf", format, desc = "Format Range", mode = "v", has = "documentRangeFormatting" },
+      -- { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v" }, has = "codeAction" },
+      -- {
+      --   "<leader>cA",
+      --   function()
+      --     vim.lsp.buf.code_action({
+      --       context = {
+      --         only = {
+      --           "source",
+      --         },
+      --         diagnostics = {},
+      --       },
+      --     })
+      --   end,
+      --   desc = "Source Action",
+      --   has = "codeAction",
+      -- }
     }
     if require("lazyvim.util").has("inc-rename.nvim") then
       M._keys[#M._keys + 1] = {
